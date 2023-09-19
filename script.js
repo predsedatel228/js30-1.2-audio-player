1d2bc947caaf6d376a24cdd090eb25315a48bdf2
Egor Panevin <predsedatel228@yandex.ru>
2023-09-19 16:40:17 +0300

    add html and CSS

---
+++ script.js
@@ -0,0 +1,3 @@
+const playButton = document.querySelector('.player__play-button');
+const songTitle = document.querySelector('.player__song-title');
+const songTitle = document.querySelector('.player__song-title');
